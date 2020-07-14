fecd8f0c1bf5bcb0c32cd89d900f0c539419853b
kamilloo <kamil.pietka85@gmail.com>
2020-07-14 21:03:46 +0200

    crawler with pagination

--- quotes_spider.py
+++ quotes_spider.py
@@ -3,19 +3,24 @@ import scrapy
 
 class QuotesSpider(scrapy.Spider):
     name = "quotes"
+    url = 'https://katalog.trojmiasto.pl/moda_i_uroda/salony_kosmetyczne'
     def start_requests(self):
-        url = 'https://katalog.infoludek.pl/kategoria/gabinety_kosmetyczne/61'
-        yield scrapy.Request(url, self.parse)
+        yield scrapy.Request(self.url, self.parse)
 
     def parse(self, response):
-        for quote in response.css('div.entry_result'):
+        for quote in response.css('div.basic-information'):
+
            yield {
-                'text': quote.css('h4 a::text').get(),
-                'author': quote.css('div.entry_data').xpath('text()').getall()[1].strip(),
+                'title': quote.css('h1 a span::text').get(),
+                'www': quote.css('div.wrap dl.ob-details dd.www').xpath('a/@href').getall(),
            }
 
-        next_pages = response.css('div.paginator a::attr(href)').getall()
-        for page in next_pages:
-            yield response.follow(page, self.parse)
+        pages = response.css('li.page-step').xpath('a')
+        page = list(filter(lambda x: x.xpath('@title').get() == 'następna', pages))[0]
+        if page is not None:
+            next_pages = page.xpath('@href').get()
+            if next_pages is not None:
+                page = self.url + next_pages
+                yield response.follow(page, self.parse)
 
 
